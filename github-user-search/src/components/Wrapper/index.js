801b64c07c13aac94487c1717e0e32a601d52a1d
antonio.meireles <edgarmeireles68@gmail.com>
2021-11-04 12:21:02 +0100

    built wrappers structure

--- github-user-search/src/components/Wrapper/index.js
+++ github-user-search/src/components/Wrapper/index.js
@@ -1,1 +1,13 @@
 import React from "react";
+import "./styles.css";
+const Wrapper = () => {
+	return (
+		<div className='main'>
+			<div className='repo-wrapper'>
+				<div className='repos'>Project</div>
+			</div>
+		</div>
+	);
+};
+
+export default Wrapper;
