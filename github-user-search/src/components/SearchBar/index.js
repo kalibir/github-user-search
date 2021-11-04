6ef9f0f3561b7a5e55d2ec7472d65a283c8a92a1
antonio.meireles <edgarmeireles68@gmail.com>
2021-11-04 13:08:40 +0100

    setup for axios GET request

--- github-user-search/src/components/SearchBar/index.js
+++ github-user-search/src/components/SearchBar/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./styles.css";
 
-const SearchBar = () => {
+const SearchBar = ({ clickHandler, searchHandler }) => {
 	return (
 		<form className='search-form'>
-			<input className='search-bar' placeholder='Search user repositories...' />
-			<button className='search-button'>Search</button>
+			<input
+				className='search-bar'
+				onChange={searchHandler}
+				placeholder='Search user repositories...'
+			/>
+			<button onClick={clickHandler} className='search-button'>
+				Search
+			</button>
 		</form>
 	);
 };
--- github-user-search/src/components/Wrapper/index.js
+++ github-user-search/src/components/Wrapper/index.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import SearchBar from "../SearchBar";
+import axios from "axios";
 
 const Wrapper = () => {
+	const [user, setUser] = useState("");
+	const [repos, setRepos] = useState("");
+
+	const handleSearch = (e) => {
+		setUser(e.target.value);
+	};
+
+	const handleResults = (e) => {
+		e.preventDefault();
+		axios
+			.get(`https://api.github.com/users/${user}/repos`)
+			.then((res) => {
+				setRepos(res.data);
+				console.log(repos);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
 	return (
 		<div className='main'>
-			<SearchBar />
-			<div className='repo-wrapper'>
-				<div className='repos'>Project</div>
-			</div>
+			<SearchBar clickHandler={handleResults} searchHandler={handleSearch} />
+			<div className='repo-wrapper'></div>
 		</div>
 	);
 };
