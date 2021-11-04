e642d9048bbc7b637b5219162f840fcef494ca58
antonio.meireles <edgarmeireles68@gmail.com>
2021-11-04 12:40:35 +0100

    styled search bar

--- github-user-search/src/components/SearchBar/index.js
+++ github-user-search/src/components/SearchBar/index.js
@@ -1,1 +1,13 @@
 import React from "react";
+import "./styles.css";
+
+const SearchBar = () => {
+	return (
+		<form className='search-form'>
+			<input className='search-bar' placeholder='Search user repositories...' />
+			<button className='search-button'>Search</button>
+		</form>
+	);
+};
+
+export default SearchBar;
--- github-user-search/src/components/Wrapper/index.js
+++ github-user-search/src/components/Wrapper/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./styles.css";
+import SearchBar from "../SearchBar";
+
 const Wrapper = () => {
 	return (
 		<div className='main'>
+			<SearchBar />
 			<div className='repo-wrapper'>
 				<div className='repos'>Project</div>
 			</div>
